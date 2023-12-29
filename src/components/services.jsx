2ec6182f579e3c3815da69a47c6697f3bca1e438
hailay12michael <haile12black@gmail.com>
2023-12-29 13:46:49 +0300

    add

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -7,8 +7,7 @@ export const Services = (props) => {
         <div className="section-title">
           <h2>Our Services</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+          The leading software  development company in Ethiopia.
           </p>
         </div>
         <div className="row">
